91854f4aea5f024295bce87e59f0fe0488cf4420
Tselmeg ADIYAMUNKH <tadiyamu@paul-f4Ar6s9.clusters.42paris.fr>
2023-07-30 16:08:35 +0200

    Valgrind error fix

--- srcs/render_map.c
+++ srcs/render_map.c
@@ -6,7 +6,7 @@
 /*   By: tadiyamu <tadiyamu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 19:11:44 by tadiyamu          #+#    #+#             */
-/*   Updated: 2023/07/30 13:55:24 by tadiyamu         ###   ########.fr       */
+/*   Updated: 2023/07/30 15:58:09 by tadiyamu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,17 @@ void	render_map(t_data *data)
 	delta_x = 150 / data->height;
 	delta_y = 150 / data->len;
 	map_x = 0;
-	while (map_x < 150 - 1)
+	while (map_x < 150)
 	{
 		map_y = 0;
-		while (map_y < 150 - 1)
+		while (map_y < 150)
 		{
 			if ((int)(map_x / delta_x) == (int) data->player.position.x
 				&& (int)(map_y / delta_y) == (int) data->player.position.y)
 				ft_mlx_pixel_put(data, map_x, map_y, 0xe63946);
-			else if (data->map[(int)(map_x / delta_x)][
+			else if ((int)(map_x / delta_x) < data->height
+					&& (int)(map_y / delta_y) < data->len
+					&& data->map[(int)(map_x / delta_x)][
 					(int)(map_y / delta_y)] == '1')
 				ft_mlx_pixel_put(data, map_x, map_y, 0xe5e5e5);
 			map_y++;
