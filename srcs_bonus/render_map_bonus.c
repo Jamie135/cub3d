943aa7422a772e792a2a40bca791e27fc234b0cf
Pomprab BUREERAK <pbureera@paul-f4Br5s3.clusters.42paris.fr>
2023-08-04 11:45:48 +0200

    minimap orientation fixed

--- srcs_bonus/render_map_bonus.c
+++ srcs_bonus/render_map_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   render_map_bonus.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tadiyamu <tadiyamu@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pbureera <pbureera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 19:11:44 by tadiyamu          #+#    #+#             */
-/*   Updated: 2023/07/30 17:29:57 by tadiyamu         ###   ########.fr       */
+/*   Updated: 2023/08/04 11:40:09 by pbureera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 void	render_map(t_data *data)
 {
-	int		map_x;
-	int		map_y;
-	double	delta_x;
-	double	delta_y;
+	int			map_x;
+	int			map_y;
+	double		delta_x;
+	double		delta_y;
 
 	delta_x = 150 / data->height;
 	delta_y = 150 / data->len;
@@ -27,14 +27,14 @@ void	render_map(t_data *data)
 		map_y = 0;
 		while (map_y < 150)
 		{
-			if ((int)(map_x / delta_x) == (int) data->player.position.x
+			if ((int)(map_x / delta_x) == (int) data->player.position.x \
 				&& (int)(map_y / delta_y) == (int) data->player.position.y)
-				ft_mlx_pixel_put(data, map_x, map_y, 0xe63946);
-			else if ((int)(map_x / delta_x) < data->height
-					&& (int)(map_y / delta_y) < data->len
-					&& data->map[(int)(map_x / delta_x)][
-					(int)(map_y / delta_y)] == '1')
-				ft_mlx_pixel_put(data, map_x, map_y, 0xe5e5e5);
+				ft_mlx_pixel_put(data, 150 - map_y, 150 - map_x, 0xe63946);
+			else if ((int)(map_x / delta_x) < data->height \
+					&& (int)(map_y / delta_y) < data->len \
+					&& data->map[(int)(map_x / delta_x)] \
+					[(int)(map_y / delta_y)] == '1')
+				ft_mlx_pixel_put(data, 150 - map_y, 150 - map_x, 0xe5e5e5);
 			map_y++;
 		}
 		map_x++;
